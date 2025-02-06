5efb5d6df1f2de0898325cf73635d6939c433887
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2025-02-06 10:03:15 -0500

    Actualizaciones, atributos y el constructor

--- src/main/java/espcoh/ec/edu/gestion/controlador/Controlador.java
+++ src/main/java/espcoh/ec/edu/gestion/controlador/Controlador.java
@@ -10,4 +10,28 @@ package espcoh.ec.edu.gestion.controlador;
  */
 public class Controlador {
     
+    //Controlador unindo lo vista y el modelo
+    
+    
+    /*private Modelo modelo;
+    private Vista vista;
+    private GestionTarea gestionTarea;
+
+    public Controlador(Modelo modelo, Vista vista, GestionTarea gestionTarea) {
+        this.modelo = modelo;
+        this.vista = vista;
+        this.gestionTarea = gestionTarea;
+    }
+    */
+    
+    
+
+   
+    
+    
+    
+    
+    
+    
+    
 }
